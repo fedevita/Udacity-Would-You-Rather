b4c56169ea313228cf0ef3d94490b45beee93ef5
Federico Vita <federico.vita1997@gmail.com>
2021-12-21 21:39:54 +0100

    fixed redirection for route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/Main";
 import SignInPage from "./pages/SignInPage";
 import { Box } from "@mui/material";
@@ -13,7 +13,7 @@ import { getUsers } from "./features/users";
 import { getQuestions } from "./features/questions";
 
 const pages = [
-  { id: "home", name: "Home" },
+  { id: "Home", name: "Home" },
   { id: "NewQuestion", name: "New Question" },
   { id: "LeaderBoard", name: "Leader Board" },
 ];
@@ -28,8 +28,9 @@ function App() {
       {islogged && <Header pages={pages} />}
       <Routes>
         {islogged && (
-          <Route path="" element={<Main />}>
-            <Route path="home" element={<Home></Home>}></Route>
+          <Route path="*" element={<Main />}>
+            <Route path="" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home></Home>}></Route>
             <Route path="NewQuestion" element={<NewQuestion />}></Route>
             <Route
               path="LeaderBoard"
@@ -38,7 +39,11 @@ function App() {
             <Route path="Profile" element={<Box>Profile</Box>}></Route>
           </Route>
         )}
-        {!islogged && <Route path="" element={<SignInPage />}></Route>}
+        {!islogged && (
+          <Route path="" element={<SignInPage />}>
+            <Route path="*" element={<Navigate to="" />} />
+          </Route>
+        )}
       </Routes>
     </>
   );
